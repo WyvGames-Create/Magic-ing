e86211d9ee73fdfafde9128f01993b6d6d690c79
Jason <jasontrollsme@gmail.com>
2025-04-24 03:27:05 -0400

    Overcooked
    Imported TMPro
    
    Added UI
    Added Collision Trigger
    Added UI Fading Script on Collision
    
    Grouped Platform and Collision to Stage

---
+++ Assets/Scripts/Collider Trigger.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class BoxTrigger : MonoBehaviour
+{
+    public Canvas screenUI;
+    public float fadeSpeed = 1;
+
+    private float _desiredAlpha;
+    private float _currentAlpha;
+
+    void Update()
+    {
+        _currentAlpha = Mathf.MoveTowards(_currentAlpha, _desiredAlpha, fadeSpeed * Time.deltaTime);
+        screenUI.GetComponent<CanvasGroup>().alpha = _currentAlpha;
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            _desiredAlpha = 1f;
+            Debug.Log("Player Entered");
+        }
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            _desiredAlpha = 0f;
+            Debug.Log("Player Exited");
+        }
+    }
+}
