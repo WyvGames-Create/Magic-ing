fac2d0fbdb15cf8cf72b70010a7d4828bd2988ef
HiThereBuddies <123123dahe@gmail.com>
2025-04-24 21:36:49 -0400

    Fix
    BasicMovement Fix

--- Assets/Scripts/BasicMovement.cs
+++ Assets/Scripts/BasicMovement.cs
@@ -9,31 +9,32 @@ public class BasicMovement : MonoBehaviour
     private float jumpingPower = 16f;
     private bool isFacingRight = true;
 
-    [SerializedField] private Rigidbody2D rb;
-    [SerializedField] private Transform groundCheck;
-    [SerializedField] private LayerMask groundLayer;
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
 
 
     // Update is called once per frame
     void Update()
     {
-        horizontal = Input.GetAxisRaw("horizontal");
+        horizontal = Input.GetAxisRaw("Horizontal");
         if (Input.GetButtonDown("Jump") && IsGrounded()){
-            rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpingPower);
         }
-        if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f){
-            rb.velocity - new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+        if (Input.GetButtonUp("Jump") && rb.linearVelocity.y > 0f){
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, rb.linearVelocity.y * 0.5f);
         }
         Flip();
     }
     private void FixedUpdate(){
-        rb.velocity = new Velocity2(horizontal * speed ,rb.velocity.y);
+        rb.linearVelocity = new Vector2(horizontal * speed ,rb.linearVelocity.y);
     }
     private bool IsGrounded(){
+        Debug.Log("Yes");
         return Physics2D.OverlapCircle(groundCheck.position, 0.2f ,groundLayer);
     } 
     private void Flip(){
-        if (isFacingRight && horizontal < 0f || !isFacingRight * horizontal > 0f){
+        if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f){
             isFacingRight = !isFacingRight;
             Vector3 localScale = transform.localScale;
             localScale.x *= -1f;
