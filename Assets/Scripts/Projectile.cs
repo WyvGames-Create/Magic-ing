562499372e2cba8a41bc7b80cbfed1988872c5ee
Jason <jasontrollsme@gmail.com>
2025-04-26 14:37:31 -0400

    Pews Now Commit Unalive
    Modified Projectiles
        - Added Lifespan
        - Added Distance Limit
        - Added Toggle between lifespan and distance
    
    Gave the script some nice looking headers

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -2,11 +2,19 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
+    [Header("Projectile Attributes")]
     public float speed = 10;
     public int bounceCount = 0;
+    [Header("Projectile Despawning")]
+    public float timeLimit = 2;
+    public bool useDistanceLimit = false;
+    public float distanceLimit = 20;
+    [Header("Debug")]
+    public bool debugCollision = false;
 
     private Rigidbody2D _rigidbody;
     private Vector2 _direction;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -15,11 +23,16 @@ public class Projectile : MonoBehaviour
         // Setting Values
         _direction = transform.right;
         _rigidbody.linearVelocity = _direction * speed;
+        // Set a timer to delete the projectile
+        Destroy(gameObject, useDistanceLimit ? distanceLimit / speed : timeLimit);
     }
 
     // Update is called once per frame
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        // Collision Debug
+        if (debugCollision) Debug.Log($"{collision.gameObject.name} was hit!");
+
         if (bounceCount > 0)
         {
             // Bounce by reflecting over the normal vector of the collided object
@@ -32,8 +45,15 @@ public class Projectile : MonoBehaviour
         } 
         else
         {
+            // Delete the projectile early should a collision be detected 
             Destroy(gameObject);
+            return;
         }
-        Debug.Log($"{collision.gameObject.name} was hit!");
+    }
+
+    // OnDisable is called on destroy
+    private void OnDisable()
+    {
+        
     }
 }
